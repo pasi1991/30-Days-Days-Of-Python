da27e0a9d61a27bfe33f0740f7584ae4e4622212
Patrick Siemsen <patrick.siemsen@t-online.de>
2023-09-07 16:07:07 +0200

    day_1

---
+++ day_1/helloworld.py
@@ -0,0 +1,55 @@
+"""
+Open the python interactive shell and do the following operations. The operands are 3 and 4.
+
+    addition(+)
+    subtraction(-)
+    multiplication(*)
+    modulus(%)
+    division(/)
+    exponential(**)
+    floor division operator(//)
+"""
+
+print(2+3)
+print(3-2)
+print(3*2)
+print(3%2)
+print(3/2)
+print(3**3)
+print(3//2)
+
+"""
+Write strings on the python interactive shell. The strings are the following:
+
+    Your name
+    Your family name
+    Your country
+    I am enjoying 30 days of python
+"""
+
+print('Patrick')
+print('Siemsen')
+print('Germany')
+print('I am enyoing 30 days of python')
+
+"""
+Check the data types of the following data:
+
+    10
+    9.8
+    3.14
+    4 - 4j
+    ['Asabeneh', 'Python', 'Finland']
+    Your name
+    Your family name
+    Your country
+"""
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 -4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Patrick'))
+print(type('Siemsen'))
+print(type('Germany'))
